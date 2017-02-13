93f45bc86808b887d44166f56627752f4df88377
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-13 16:30:25 +0900

    [Init] Gradle Project

---
+++ src/main/java/com/masahirosaito/spigot/zenbuhoru/ZenbuHoru.kt
@@ -0,0 +1,15 @@
+package com.masahirosaito.spigot.zenbuhoru
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ZenbuHoru : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
